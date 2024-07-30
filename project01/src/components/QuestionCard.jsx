8471cadf024c31ae314e68a83a15af278b3a3b74
Vivek Yadav <85821224+TheVivekYadav@users.noreply.github.com>
2024-07-30 16:29:52 +0530

    Update QuestionCard.jsx

--- project01/src/components/QuestionCard.jsx
+++ project01/src/components/QuestionCard.jsx
@@ -82,7 +82,7 @@ function QuestionCard(props){
     }
 
     if (error){
-        return <div>Something Wen't Worng<br/>or Contact Vivek Yadav to active on backend</div>
+        return <div class="error">Something Wen't Worng<br/>or Contact Vivek Yadav to make backend active  </div>
     }
     
 
