9b39f229c0bc9a3b3974abbde479955b20cc8705
Vivek Yadav <85821224+TheVivekYadav@users.noreply.github.com>
2024-07-30 15:39:12 +0530

    Update vite.config.js

--- project01/vite.config.js
+++ project01/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server: {
     proxy:{
-      '/api': 'http://localhost:8080'
+      '/api': 'http://api-backend-quiz-project04.apps.ap46r.prod.ole.redhat.com/'
     }
   },
   plugins: [react()],
